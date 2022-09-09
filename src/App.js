be8061d296618c81f4bd61307ec354db002e3872
jellyfish28 <gkonuhovam@gmail.com>
2022-09-09 15:06:03 +0300

    Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import logo from './logo.png';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="container">
+        <header className="App-header">
 
-        <img src={logo} className="App-logo img-fluid" />
-      </header>
+          <img src={logo} className="App-logo img-fluid" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by AnnaS</footer>
+      </div>
     </div>
   );
 }
