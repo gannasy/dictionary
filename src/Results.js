462cb07710ca1487ae7f6aab74d928c9b169511d
jellyfish28 <gkonuhovam@gmail.com>
2022-09-13 15:20:51 +0300

    Phonetics

--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Meaning from './Meaning';
+import Phonetic from './Phonetic';
 
 export default function Results(props) {
 
     if (props.description) {
         return (<div className="Results">
             <h2>{props.description.word}</h2>
-            <h2>{props.description.phonetic}</h2>
+            {props.description.phonetics.map(function (phonetic, index) {
+                return (
+                    <div key={index}>
+                        <Phonetic phonetic={phonetic} />
+                    </div>
+                )
+            })}
             {props.description.meanings.map(function (meaning, index) {
                 return (<div key={index}>
                     <Meaning meaning={meaning} />
