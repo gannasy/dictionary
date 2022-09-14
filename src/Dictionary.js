4ed998cc73ac8497f0513ac1f3743a80686a19f0
jellyfish28 <gkonuhovam@gmail.com>
2022-09-14 18:28:41 +0300

    photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,22 +2,35 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './Dictionary.css';
 import Results from './Results';
+import Photos from './Photos';
 
 function Dictionary(props) {
     let [keyword, setKeyword] = useState(props.defaultkey);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
     function handleResponse(response) {
-        console.log(response);
-
-
         setResults(response.data[0]);
+    }
+
+    function handlePexelResponse(response) {
+        setPhotos(response.data.photos);
     }
 
     function search() {
         let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(url).then(handleResponse);
+
+        let pexelKey = '563492ad6f91700001000001fa46043ef3fd42199363d8d1c088ad4f';
+
+        let pexelsUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+        let headers = { Authorization: `Bearer ${pexelKey}` };
+
+        axios.get(pexelsUrl, {
+            headers: headers
+        }).then(handlePexelResponse);
+
     }
 
     function handleSubmit(event) {
@@ -45,6 +58,7 @@ function Dictionary(props) {
                 </form>
                 <div className="hint">suggested words: wine, yoga, plan</div>
                 <Results description={results} />
+                <Photos photos={photos} />
             </section>
         </div>)
     } else {
--- src/Meaning.js
+++ src/Meaning.js
@@ -3,7 +3,7 @@ import Synonims from './Synonims';
 import './Meaning.css';
 
 export default function Meaning(props) {
-    console.log(props.meaning);
+
     return (
         <div className="Meaning">
             <h3>{props.meaning.partOfSpeech}</h3>
